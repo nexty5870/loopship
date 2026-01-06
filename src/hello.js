40e7cc3c82fe1837ad753ac23ed209ae49d332ca
Quentin <quentin.daems@gmail.com>
2026-01-06 22:54:45 +0100

    feat: add hello module with greet function

---
+++ src/hello.js
@@ -0,0 +1,8 @@
+/**
+ * Greet someone by name
+ * @param {string} name - The name to greet
+ * @returns {string} The greeting message
+ */
+export function greet(name) {
+  return `Hello, ${name}!`;
+}
